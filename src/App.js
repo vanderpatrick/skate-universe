aa0b4e4d6536665f58de945f14461d5929725ba5
vanderpatrick <patrick123321123@icloud.com>
2022-12-12 16:35:01 +0000

    Add utils, navbar placeholder and connect to api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import styles from './App.module.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className={styles.App}>
-      
+        <NavBar />
     </div>
   );
 }
