f1c313d23195e4005ceb5bcaf04e3b732c7e7964
vanderpatrick <patrick123321123@icloud.com>
2022-12-15 00:13:48 +0000

    Add video comment form

---
+++ src/pages/videoComments/VideoCommentCreateForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+function VideoCommentCreateForm(props) {
+  const { video, setVideo, setVideoComments, profileImage, profile_id } = props;
+  const [content, setContent] = useState("");
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/videocomments/", {
+        content,
+        video,
+      });
+      setComments((setVideoComments) => ({
+        ...setVideoComments,
+        results: [data, ...setVideoComments.results],
+      }));
+      setVideo((prevVideo) => ({
+        results: [
+          {
+            ...prevVideo.results[0],
+            video_comments: prevVideo.results[0].video_comments + 1,
+          },
+        ],
+      }));
+      setContent("");
+    } catch (err) {
+      // console.log(err);
+    }
+  };
+
+  return (
+    <Form className="mt-2" onSubmit={handleSubmit}>
+      <Form.Group>
+        <InputGroup>
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profileImage} />
+          </Link>
+          <Form.Control
+            className={styles.Form}
+            placeholder="my comment..."
+            as="textarea"
+            value={content}
+            onChange={handleChange}
+            rows={2}
+          />
+        </InputGroup>
+      </Form.Group>
+      <button
+        className={`${styles.Button} btn d-block ml-auto`}
+        disabled={!content.trim()}
+        type="submit"
+      >
+        video
+      </button>
+    </Form>
+  );
+}
+
+export default VideoCommentCreateForm;
